3fd5b1dfb1f459a63af44de29bff941bd9887162
James Gardiner <jamesgardiner075@gmail.com>
2022-06-22 19:55:02 +0100

    Fix tests running twice

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const nextJest = require("next/jest")
 const createJestConfig = nextJest()
 
 module.exports = createJestConfig({
-    moduleDirectories: ["node_modules", "<rootDir>/"],
+    moduleDirectories: ["node_modules", "<rootDir>/src/__tests__"],
     testEnvironment: "jest-environment-jsdom",
     setupFilesAfterEnv: ["./jest.setup.ts"],
 })
